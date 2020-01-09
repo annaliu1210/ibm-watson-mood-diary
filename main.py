9fabe7c412ae21437f81f085251034a86ad7c988
Anna Liu <annaliu1210@gmail.com>
2020-01-08 20:39:14 -0500

    Added results page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from config import Config
 from forms import DiaryEntry
 
@@ -12,7 +12,18 @@ def index():
     Return the home page.
     """
     form = DiaryEntry()
+    if form.validate_on_submit():
+        diary = form.text.data
+        return redirect(url_for('results', diary=diary))
     return render_template('index.html', form=form)
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    """
+    Return the results.
+    """
+    return render_template('results.html', diary=request.args.get('diary'))
 
 
 if __name__ == "__main__":
